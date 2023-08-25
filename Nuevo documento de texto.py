76cba6b896010e72c98f56826bf56e8f92b2c264
S12-Chawis <santiagovillapanesso1@gmail.com>
2023-08-25 11:16:27 -0500

    nt4  se estudiaron ciclos y vimos inicios de array

---
+++ Nuevo documento de texto.py
@@ -0,0 +1,44 @@
+peluches=["roshi","vegeta","kakaroto"]
+
+opcion=0
+
+print("pelucheria PELUCHES")
+print("....................")
+print("1. agregar producto ala bodega")
+print("2. ver inventario")
+print("3. calcular valor del inventario")
+print("4. ver productos mas vendidos")
+
+while(opcion!=5):
+    
+    opcion=int(input("ingrese un numero: "))
+    print("....................")
+    if(opcion==1):
+        print("usted es en la opcion 1")
+        print("....................")
+    elif(opcion==2):
+        print("usted esta en la opcion 2")
+        print("....................")
+    elif(opcion==3):
+        print("usted es en la opcion 3")
+        print("....................")
+    elif(opcion==4):
+        print("usted esta en la opcion 4")
+        print("....................")
+    elif(opcion==5):
+        
+        print("hasta luego")    
+    else:
+        print("esta opcion no existe")
+        print("....................")                
+print("sali del ciclo")
+
+
+
+
+
+
+
+
+
+
